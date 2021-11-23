6e192ac1f9f536e1f2c08168b04dbe252af493ea
Dan <firelaff@gmail.com>
2021-11-23 22:52:23 +0200

    Authentication Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginForm from "./components/LoginForm.js";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function App() {
+  let theme = createTheme({
+    palette: {
+      primary: {
+        dark: "#2e7031",
+        main: "#43a047",
+        light: "#68b36b",
+        contrastText: "#000",
+      },
+      secondary: {
+        dark: "#00a152",
+        main: "#00e676",
+        light: "#33eb91",
+        contrastText: "#000",
+      },
+    },
+    components: {
+      MuiInputLabel: {
+        styleOverrides: {
+          root: {
+            fontWeight: "bold",
+          },
+        },
+      },
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            fontWeight: "bold",
+          },
+        },
+      },
+      MuiInput: {
+        styleOverrides: {
+          root: {
+            fontWeight: "bold",
+          },
+        },
+      },
+    },
+  });
+
+  theme = createTheme(theme, {
+    palette: {
+      info: {
+        main: theme.palette.secondary.main,
+      },
+    },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <LoginForm />
+      </ThemeProvider>
     </div>
   );
 }
