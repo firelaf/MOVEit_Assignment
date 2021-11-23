a15027a591aac7456f0427ab4823bc930275ec87
Dan <firelaff@gmail.com>
2021-11-23 22:53:12 +0200

    Authentication Done

---
+++ src/components/LoginForm.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useRef } from "react";
+
+import FormControl from "@mui/material/FormControl";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import Button from "@mui/material/Button";
+
+const LoginForm = () => {
+  const username = useRef();
+  const password = useRef();
+
+  async function requestToken(user, pass) {
+    const reqBody = `grant_type=password&username=${user}&password=${pass}`;
+    const response = await fetch(
+      "https://mobile-1.moveitcloud.com/api/v1/token",
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: reqBody,
+      }
+    );
+    console.log(user, pass);
+
+    const data = await response.json();
+    return data;
+  }
+
+  return (
+    <div>
+      <form
+        id="login"
+        onSubmit={(event) => {
+          event.preventDefault();
+          requestToken(username.current.value, password.current.value).then(
+            (data) => {
+              console.log(data);
+            }
+          );
+        }}
+      >
+        <FormControl color="primary" required={true}>
+          <InputLabel htmlFor="user">User Name</InputLabel>
+          <Input id="user" aria-describedby="username" inputRef={username} />
+        </FormControl>
+        <FormControl color="primary" required={true}>
+          <InputLabel htmlFor="pass">Password</InputLabel>
+          <Input id="pass" aria-describedby="password" inputRef={password} />
+        </FormControl>
+        <Button type="submit">Login</Button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
